ac6f8d6a36a442edcfebb451810beeceff5f9c57
eliezerangel <eliezerangel@gmail.com>
2024-08-05 20:01:16 +0100

    sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 echo "Running composer"
+cp /etc/secrets/.env .env
 composer global require hirak/prestissimo
-#composer install --no-dev --working-dir=/var/www/html
+composer install --no-dev --working-dir=/var/www/html
 
-# npm install
-# npm run dev
-
-echo "generating application key..."
-php artisan key:generate --show
+echo "Clearing caches..."
+php artisan optimize:clear
 
 echo "Caching config..."
 php artisan config:cache
@@ -17,3 +15,5 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "done deploying"
